f13595198ade1bf055268a36833ee7cbe1c436e2
ReactiioN1337 <reactiion@haze-productions.com>
2017-01-19 16:25:04 +0100

    Added stdafx.hpp

---
+++ client/include/stdafx.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+/// std::array
+#include <array>
+/// int64_t
+#include <cstdint>
+/// std::map
+#include <map>
+/// std::stringstream
+#include <sstream>
+/// std::string
+#include <string>
+/// std::vector
+#include <vector>
+/// nlohmann::json
+#include <json.hpp>
+/// tfm::format, tfm::printf, tfm::printIn
+#include <tinyformat.hpp>
