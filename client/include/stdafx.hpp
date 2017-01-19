ad6fccca8340396c655bac146cd9c47312f44f40
ReactiioN1337 <reactiion@haze-productions.com>
2017-01-19 22:28:08 +0100

    Reformat for static linking.

--- client/include/stdafx.hpp
+++ client/include/stdafx.hpp
@@ -1,4 +1,7 @@
 #pragma once
+/// Enable this define as far as you link the client static
+#define CLIENT_STATIC_LIB
+
 #ifdef _MSC_VER
 /// Otherwise min/max in json won't work without a minimal modification.
 #define NOMINMAX
@@ -25,11 +28,23 @@
 #include <string>
 /// std::vector
 #include <vector>
-/// nlohmann::json
-#include <json.hpp>
-/// tfm::format, tfm::printf, tfm::printIn
-#include <tinyformat.hpp>
 
+#if defined(_MSC_VER) && defined(CLIENT_STATIC_LIB)
+/// crypto++ include files
+#include "../contrib/cryptopp565/crypto++/cryptlib.h"
+#include "../contrib/cryptopp565/crypto++/aes.h"
+#include "../contrib/cryptopp565/crypto++/base64.h"
+#include "../contrib/cryptopp565/crypto++/filters.h"
+#include "../contrib/cryptopp565/crypto++/modes.h"
+#include "../contrib/cryptopp565/crypto++/hex.h"
+#include "../contrib/cryptopp565/crypto++/osrng.h"
+#include "../contrib/cryptopp565/crypto++/rsa.h"
+#include "../contrib/cryptopp565/crypto++/gcm.h"
+/// nlohmann::json
+#include <../contrib/nlohmann/json.hpp>
+/// tfm::format, tfm::printf, tfm::printIn
+#include <../contrib/tinyformat/tinyformat.hpp>
+#else
 /// crypto++ include files
 #include <crypto++/cryptlib.h>
 #include <crypto++/aes.h>
@@ -40,3 +55,9 @@
 #include <crypto++/osrng.h>
 #include <crypto++/rsa.h>
 #include <crypto++/gcm.h>
+/// nlohmann::json
+#include <json.hpp>
+/// tfm::format, tfm::printf, tfm::printIn
+#include <tinyformat.hpp>
+
+#endif
